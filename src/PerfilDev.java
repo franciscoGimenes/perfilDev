c730c4c13d10d6e0528a8466e00aa51f4b3f4e46
Francisco <f.gimenes1316@gmail.com>
2026-03-10 13:26:40 -0300

    Adicionando questão sobre experiencia

--- src/PerfilDev.java
+++ src/PerfilDev.java
@@ -16,10 +16,16 @@ public class PerfilDev {
         String gostaPOO = scanner.nextBoolean() ? "gosta" : "não gosta";
         scanner.nextLine(); // Consumir a quebra de linha pendente
 
+        System.out.println("Você tem experiencia na area de desenvolvimento? (true/false): ");
+        boolean temExperiencia = scanner.nextBoolean();
+        String experiencia = temExperiencia ? "tem" : "não tem";
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
         System.out.print("Digite sua linguagem de programação favorita: ");
         String linguagemFavorita = scanner.nextLine();
 
-        System.out.printf("Olá, %s! Você tem %d anos, %s de programação orientada a objetos e sua linguagem de programação favorita é %s.%n", nome, idade, gostaPOO, linguagemFavorita);
+        System.out.printf("Olá, %s! Você tem %d anos, %s de programação orientada a objetos, %s experiencia na área de desenvolvimento e sua linguagem de programação favorita é %s.%n",
+                nome, idade, gostaPOO, experiencia, linguagemFavorita);
 
         scanner.close();
     }
