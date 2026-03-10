3c143e3f9eaeb11cab1ffc2c545550263d89bda3
Francisco <f.gimenes1316@gmail.com>
2026-03-10 12:04:53 -0300

    Adicionando questão sobre POO

--- src/PerfilDev.java
+++ src/PerfilDev.java
@@ -10,13 +10,16 @@ public class PerfilDev {
 
         System.out.print("Digite sua idade: ");
         int idade = scanner.nextInt();
-        scanner.nextLine(); // Consumir a quebra de linha
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+
+        System.out.print("Você gosta de progamação orientada a objetos? (true/false): ");
+        String gostaPOO = scanner.nextBoolean() ? "gosta" : "não gosta";
+        scanner.nextLine(); // Consumir a quebra de linha pendente
 
         System.out.print("Digite sua linguagem de programação favorita: ");
         String linguagemFavorita = scanner.nextLine();
 
-        System.out.printf("Olá, %s! Você tem %d anos e sua linguagem de programação favorita é %s.%n",
-                nome, idade, linguagemFavorita);
+        System.out.printf("Olá, %s! Você tem %d anos, %s de programação orientada a objetos e sua linguagem de programação favorita é %s.%n", nome, idade, gostaPOO, linguagemFavorita);
 
         scanner.close();
     }
